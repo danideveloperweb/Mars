5f55819447399d745c642362ae642ba5b6ba03e9
danideveloperweb <danideveloperweb@gmail.com>
2022-01-24 08:24:36 +0100

    mars3

--- src/app/components/marte/marte.component.ts
+++ src/app/components/marte/marte.component.ts
@@ -7,8 +7,30 @@ import { Component } from '@angular/core';
 })
 export class MarteComponent {
   mart = {
-    move: 'grid-column:1;grid-row:1',
+    coche:'grid-column:1;grid-row:1'
   };
 
-  ruta = [this.mart];
+  move = 'grid-column:3;grid-row:1';
+  move2 = 'grid-column:1;grid-row:3';
+  move3 = 'grid-column:5;grid-row:5';
+
+
+
+  moveRuta() {
+    if(this.mart.coche === this.move) {
+      alert('Cuidado !!! Un Alien escondamonos , tiene pinta de hostil');
+      return this.mart.coche = 'grid-column:2;grid-row:1';
+    }
+    if(this.mart.coche === this.move2) {
+      alert('Cuidado !!! Un Ovni escondamonos para que no nos detecten');
+      return this.mart.coche = 'grid-column:1;grid-row:2';
+    }
+    if(this.mart.coche === this.move3) {
+      alert('Hemos llegado a la nave, salgamos de este planeta');
+
+      return (this.mart.coche = 'grid-column:6;grid-row:6') ;
+    }
+  }
+
+
 }
